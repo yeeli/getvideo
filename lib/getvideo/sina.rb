a396eb2fd7628163f7e2da5783c6f03e7f69ac0b
yee.li <yeeli@outlook.com>
2014-06-29 20:32:01 +0800

    Fixed #5 read sina video error

--- lib/getvideo/sina.rb
+++ lib/getvideo/sina.rb
@@ -1,187 +1,94 @@
 #coding:utf-8
+require 'digest/md5'
 
 module Getvideo
   class Sina < Video
-    set_api_uri { "http://v.iask.com/v_play.php?vid=#{id}" }
-
-    def initialize(url)
-      @url = url
-      set_id
+    set_api_uri do
+      rand = "0.#{rand(10000..10000000)}#{rand(10000..10000000)}"
+      "http://v.iask.com/v_play.php?vid=#{id}&ran=#{rand}&p=i&k=#{get_k(id, rand)}" 
     end
 
     def ipad_response
-      @ipad_response ||= ipad_connect
+      @ipad_response ||= ipad_connection
     end
 
-    def info_response
-      @info_response ||= Nokogiri::HTML(parse_html).css("head script").text.gsub(" ", "")
-    end
-
-    def html_url
-      if has_html?
-        if url =~ /\.html$/
-          url
-        else
-          "http://video.sina.com.cn/v/b/#{@id}-#{@uid}.html"
-        end
-      else
-        ""
-      end
-    end
-
-    def title
-      if has_html?
-        info_response.scan(/title:'([^']+)'/)[0][0]
-      else
-        response["vname"]
-      end
-    end
-
-    def cover
-      if has_html?
-        pic = info_response.scan(/pic:'([^']+)'/)
-        pic.empty? ? "" : pic[0][0]
-      else
-        ""
-      end
-    end
-
-    def flash
-      if url =~ /\.swf/
-        url
-      else
-        flash_url = info_response.scan(/swfOutsideUrl:'([^']+)'/)
-        unless flash_url.empty?
-          flash_url[0][0]
-        else
-          ""
-        end
-      end
-    end
-
-    def m3u8
-      if has_html?
-        if !ipad_response.nil? 
-          media["mp4"][0]
-        end
-      else
-        ""
-      end
+    def initialize(url)
+      super
+      parse_page if !(is_flash_url? || is_subject_url?)
     end
 
     def id
-      @id
+      return url.match(/vid=(\d+)/).to_a[1] if is_flash_url?
+      return url.match(/#(\d+)/).to_a[1] if is_subject_url?
+      page.match(/vid[\s]*:[\s]*["|']?[\s]*(\d+)[\s]*["|']?/).to_a[1]
+    end
+
+    def cover
+      return "" if is_flash_url? || is_subject_url?
+      page.match(/pic[\s]*:[\s]*["|']?[\s]*([^'|"]+)?/).to_a[1]
+    end
+
+    def html_url
+      return "" if is_flash_url?
+      url
+    end
+
+    def title
+      response["video"]["vname"]
+    end
+
+    def flash
+      return url if is_flash_url?
+      return "" if is_subject_url?
+      return "" if !page
+      page.match(/swfOutsideUrl[\s]*:[\s]*["|']?[\s]*([^'|"]+)/).to_a[1]
+    end
+
+    def m3u8
+      if !(is_flash_url? || is_subject_url?)
+        ipad_response["video"]["durl"]["url"]
+      else
+        ""
+      end
     end
 
     def media
-      vedio_list = {}
-      vedio_list["hlv"] = []
-      vedio_list["mp4"] = []
-      if res = response
-        if res["video"]["result"] != "error"
-          vedio_list["hlv"] << res["video"]["durl"]["url"]
-        end
+      video_list = {}
+      if !(is_flash_url? || is_subject_url?)
+        video_list["mp4"] = []
+        video_list["mp4"] << ipad_response["video"]["durl"]["url"]
       end
-
-      if m_res = ipad_response
-        if m_res["video"]["result"] != "error"
-          vedio_list["mp4"] << m_res["vdieo"]["durl"]["url"]
-        end
-      end
-
-      return vedio_list
+      video_list["hlv"] = []
+      video_list["hlv"] << response["video"]["durl"]["url"]
+      return video_list
     end
 
     private
 
-    def has_html?
-      @uid =~ /[\d]{3,}?/ || (url =~ /\.html/)
+    def ipad_connection
+      rand = "0.#{rand(10000..10000000)}#{rand(10000..10000000)}"
+      Response.new(Faraday.get("http://v.iask.com/v_play.php?vid=#{ipad_vid}&ran=#{rand}&p=i&k=#{get_k(ipad_vid, rand)}")).parsed
     end
 
-    def set_id
-      if url =~ /\/v\/b\/([^\D]+)-([^\D]+)\.html/
-        if ids =  url.scan(/\/v\/b\/([^\D]+)-([^\D]+)\.html/)[0]
-          @id = ids[0]
-          @uid = ids[1]
-        end
-      elsif url =~ /\/playlist\/([^\D]+).*.#([^\D]+)/
-        if ids = url.scan(/\/playlist\/[^\D]+-([^\D]+).*#([^\D]+)/)[0]
-          @id = ids[1]
-          @uid = ids[0]
-        end
-      elsif url =~ /\.swf/
-        if ids = url.scan(/vid=([^\D]+)_([^\D]+)_.+\.swf/)[0]
-          @id = ids[0]
-          @uid = ids[1]
-        end
-      elsif url =~ /(\/[\S]?\/)/
-        if url.index("#").nil?
-          html = parse_html
-          if uids = html.scan(/uid[\s]?:[\s]?['|"]([^\D]+)['|"]/)[0]
-            @uid = uids[0]
-          end
-          if ids = html.scan(/vid[\s]?:[\s]?['|"]([^\D]+)['|"]/)[0]
-            @id = ids[0]
-          end
-        else
-          if ids = url.scan(/(\/[\S]?\/).+#([^\D]+)/)[0]
-            @id = ids[1]
-          end
-        end
-      else
-        if ids = url.split("|")
-          @id = ids[0]
-          @uid = ids[1]
-        end
-      end
+    def get_k(vid, rand)
+      t = Time.now.to_i.to_s(2)[0..-7].to_i(2)
+      Digest::MD5.hexdigest("#{vid.to_s}Z6prk18aWxP278cVAH#{t.to_s}#{rand.to_s}".encode('utf-8'))[0..15] + t.to_s
     end
 
-    def ipad_id
-      ipad_ids = info_response.scan(/videoData:{ipad_vid:[']?([^\D]+)[']?/)
-      ipad_ids.empty? ? nil : ipad_ids[0][0]
+    def is_flash_url?
+      url =~ /\.swf/ 
     end
 
-    def ipad_connect
-      conn = Faraday.new
-      response = conn.get "http://v.iask.com/v_play.php?vid=#{ipad_id}"
-      Response.new(response).parsed
+    def is_subject_url?
+      url =~ /#\d+/
     end
 
-    def html_info_path
-      if url =~ /\.html/
-        if url.index("#")
-          if url =~ /play_list/
-            html_url
-          else
-            url
-          end
-        else
-          url
-        end
-      elsif url =~ /\.swf/
-        html_url
-      else
-        html_url
-      end
+    def parse_page
+      page_res = Faraday.get(url)
+      @page = page_res.body
+      @ipad_vid = page.match(/ipad_vid[\s]*:[\s]*["|']?[\s]*(\d+)[\s]*["|']?/).to_a[1]
     end
 
-    def parse_html
-      if !html_info_path.empty?
-        conn = Faraday.new
-        conn.get(html_info_path).body
-      end
-    end
-
-    def get_media(type=nil)
-      if type == "mp4"
-        if ipad_id
-          uri = "http://v.iask.com/v_play.php?vid=#{ipad_id}"
-        end
-      else
-        uri = "http://v.iask.com/v_play.php?vid=#{id}"
-      end
-      conn = Faraday.new
-      return conn.get(uri).body
-    end
+    attr_accessor :page, :ipad_vid
   end
 end
